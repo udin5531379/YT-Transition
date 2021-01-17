b9a14f0c4bc3bc0bfb69e372c2c963ae427d0cad
Udin Rajkarnikar <udinrajkarnikar@MacBook-Pro.local>
2021-01-17 16:50:53 +0545

    first

---
+++ YT Mini Player Transition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YT Mini Player Transition
+//
+//  Created by Udin Rajkarnikar on 1/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YT Mini Player Transition/YT_Mini_Player_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_Mini_Player_TransitionApp.swift
+//  YT Mini Player Transition
+//
+//  Created by Udin Rajkarnikar on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct YT_Mini_Player_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
